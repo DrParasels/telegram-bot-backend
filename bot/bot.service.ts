7ae523d43c82c37ab2beda4d8b0a7244d23cabd2
DinoPK <fomishov121@yandex.ru>
2026-01-11 19:54:52 +0300

    format doc

--- bot/bot.service.ts
+++ bot/bot.service.ts
@@ -5,33 +5,33 @@ import { WELCOME_MESSAGE, COMMANDS_LIST } from './bot.constants';
 
 @Injectable()
 export class BotService implements OnModuleInit {
-  private bot: Telegraf<any>;
+    private bot: Telegraf<any>;
 
-  constructor(private configService: ConfigService) {}
+    constructor(private configService: ConfigService) { }
 
-  onModuleInit() {
-    const token = this.configService.get<string>('BOT_TOKEN');
-    if (!token) throw new Error('BOT_TOKEN не задан в .env');
+    onModuleInit() {
+        const token = this.configService.get<string>('BOT_TOKEN');
+        if (!token) throw new Error('BOT_TOKEN не задан в .env');
 
-    this.bot = new Telegraf(token);
+        this.bot = new Telegraf(token);
 
-    // Команда /start
+        // Команда /start
 
-this.bot.start(async (ctx) => {
-  // Отправляем картинку
-  await ctx.replyWithPhoto('blob:https://web.telegram.org/a00fca3d-35c5-4943-826d-a313182fa627');
+        this.bot.start(async (ctx) => {
+            // Отправляем картинку
+            await ctx.replyWithPhoto('blob:https://web.telegram.org/a00fca3d-35c5-4943-826d-a313182fa627');
 
-  // Отправляем текст
-  await ctx.reply(WELCOME_MESSAGE);
-  await ctx.reply('Доступные команды:\n' + COMMANDS_LIST.join('\n'));
-});
+            // Отправляем текст
+            await ctx.reply(WELCOME_MESSAGE);
+            await ctx.reply('Доступные команды:\n' + COMMANDS_LIST.join('\n'));
+        });
 
-    // Заглушка для всех сообщений
-    this.bot.on('message', (ctx) => {
-      ctx.reply('Команда заглушка. Пока что бот реагирует только на /start.');
-    });
+        // Заглушка для всех сообщений
+        this.bot.on('message', (ctx) => {
+            ctx.reply('Команда заглушка. Пока что бот реагирует только на /start.');
+        });
 
-    // Запуск бота
-    this.bot.launch().then(() => console.log('Telegram Bot запущен 🚀'));
-  }
+        // Запуск бота
+        this.bot.launch().then(() => console.log('Telegram Bot запущен 🚀'));
+    }
 }
